1c32e9fdc6530640650e53b054a54114b3562166
Lucas Noe da Silva <lucazsilvasp@gmail.com>
2025-08-07 23:55:59 -0300

    aula00

---
+++ aulas/aula00.py
@@ -0,0 +1,11 @@
+nome = "Lucas"
+cor_favorita = "preta"
+idade = 24
+
+print("Boa noite, eu me chamo",nome)
+print("Minha cor favorita é: ",cor_favorita)
+
+if idade >= 18:
+    print("Eu ja posso dirigir")
+else:
+    print("Não posso dirigir")    
